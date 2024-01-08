7779e2cf588d042c4007763ab701ed369bb2f602
maxs24 <ksenofontov-maksim@mail.ru>
2024-01-08 13:37:57 +0300

    Initial commit 2

---
+++ src/models/model.py
@@ -0,0 +1,15 @@
+from pydantic import BaseModel
+
+
+class Todo(BaseModel):
+    id: int
+    item: str
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "id": 1,
+                "item": "Example text"
+            }
+        }
+
